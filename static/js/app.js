0a17cd9ae870c9f35cfb5afed6c871b3339bd4a8
Sasankkoyi <sasankk9515@gmail.com>
2024-12-15 07:51:53 +0530

    first

---
+++ static/js/app.js
@@ -0,0 +1,12 @@
+
+//Message/notification Timer
+
+var message_timeout = document.getElementById("message-timer");
+
+setTimeout(function()
+
+{
+
+    message_timeout.style.display = "none";
+
+}, 5000);
